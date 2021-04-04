283d80318657fc17fac0bf3222003c12152594b7
Kadum Komut <34484927+kadmon47@users.noreply.github.com>
2021-04-04 10:13:11 +0530

    Create NearlyLuckyNumber.java

---
+++ NearlyLuckyNumber.java
@@ -0,0 +1,66 @@
+//package com.company;
+
+// Working program with FastReader
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    public static void main(String[] args)throws IOException {
+        FastReader s = new FastReader();
+        long n = s.nextLong();
+        int count = 0;
+        while(n>0){
+            if(n%10==4||n%10==7)
+                count++;
+            n/=10;
+        }
+        if(count==4||count==7) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
